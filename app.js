151e292f4450e06b64167a48e3898295351ea64e
codearct <codearct@gmail.com>
2022-11-19 21:51:20 +0300

    Created and got photo infos  and rendered at front

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+
 const path = require('path');
 
+const Photo = require('./models/Photo');
+
 const app = express();
+
+//Connect Db
+mongoose.connect('mongodb://127.0.0.1:27017/pcat');
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find();
+  res.render('index', {
+    photos,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -21,6 +33,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 1923;
